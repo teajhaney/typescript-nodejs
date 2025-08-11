88c09b07ce17bcfb7ba5ad3ff93fde8565d2ba0c
Yusuf <teajhaney@gmail.com>
2025-08-11 15:07:33 +0100

    basic redis

--- redis/server.js
+++ redis/server.js
@@ -1,8 +1,45 @@
-import Redis from 'redis';
-
-const redis = Redis();
+import redis from 'redis';
 
 const client = redis.createClient({
-	host: 'localhost',
-	port: 6379
+  host: 'localhost',
+  port: 6379,
 });
+
+//event listener
+
+client.on('error', error => console.log('Redis client error occured!', error));
+
+async function testREdisConnection() {
+  try {
+    await client.connect();
+    console.log('Connected to redis');
+
+    //set a value with a key
+    await client.set('name', 'yusuf');
+
+    //get thet value with the key
+    const extractValue = await client.get('name');
+    console.log(extractValue);
+
+    //delete with a key
+    const deleteValue = await client.del('name');
+    console.log(deleteValue);
+
+    const extractUpdatedValue = await client.get('name');
+    console.log(extractUpdatedValue);
+
+    //increament and decreament value
+    await client.set('count', '100');
+    const incrementCount = await client.incr('count');
+    console.log(incrementCount);
+    const decrementCount = await client.decr('count');
+    console.log(decrementCount);
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await client.quit();
+    console.log('Disconnected from redis');
+  }
+}
+
+testREdisConnection();
